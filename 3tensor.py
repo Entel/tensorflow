9c4504a526b4e17a1774148987377dbb0ef754eb
Entel <entel.science@gmail.com>
2017-05-22 14:15:43 +0900

    201705221455

--- 3tensor.py
+++ 3tensor.py
@@ -73,8 +73,8 @@ def max_pool_2x2(x):
 			padding="SAME")
 
 #tensor 1
-w_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
+w_conv1 = weight_variable([5, 5, 1, 16])
+b_conv1 = bias_variable([16])
 
 x_image = tf.reshape(x, [-1, 28, 28, 1])
 
@@ -82,16 +82,22 @@ h_conv1 = tf.nn.relu(conv2d(x_image, w_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 #tensor 2
-w_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+w_conv2 = weight_variable([5, 5, 16, 32])
+b_conv2 = bias_variable([32])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, w_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
+#tensor 3
+w_conv3 = weight_variable([5, 5, 32, 64])
+b_conv3 = bias_variable([64])
+
+h_conv3 = tf.nn.relu(conv2d(h_pool2, w_conv3) + b_conv3)
+
 w_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_pool2_flat = tf.reshape(h_conv3, [-1, 7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, w_fc1) + b_fc1)
 
 keep_prob = tf.placeholder("float")
@@ -108,7 +114,7 @@ correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 sess.run(tf.initialize_all_variables())
 
-df = open("2tensor.dat", "w+")
+df = open("3tensor.dat", "w+")
 data = []
 
 for i in range(20000):
